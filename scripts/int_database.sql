376c6b41ba366dcfb66db86e0a8b8af56b08ada7
leduyphung41-sudo <leduyphung41@gmail.com>
2026-05-10 11:33:29 +1000

    Create DataWarehouse and associated schemas
    This script creates a new database named 'DataWarehouse' along with three schemas: bronze, silver, and gold. It drops the existing database and schemas if they exist before recreating them.

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,1 +1,28 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'.
+    If the database already exists, it is dropped and recreated.
+    It also creates three schemas/databases: bronze, silver, and gold.
 
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists.
+    All data will be permanently deleted.
+=============================================================
+*/
+
+DROP DATABASE IF EXISTS DataWarehouse;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+DROP DATABASE IF EXISTS bronze;
+DROP DATABASE IF EXISTS silver;
+DROP DATABASE IF EXISTS gold;
+
+CREATE DATABASE bronze;
+CREATE DATABASE silver;
+CREATE DATABASE gold;
